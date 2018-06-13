e9aecc6bc37fd375853e5b467a5b99d118624fd2
Starfwy <starfwy@hotmail.com>
2018-06-13 14:13:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/star/movie4share/Movie4ShareApplication.java
@@ -0,0 +1,14 @@
+package com.example.star.movie4share;
+
+import android.app.Application;
+
+/**
+ * Created by Star on 2018/6/13.
+ */
+
+public class Movie4ShareApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
